6e4e855d1b0ba7c2447b2b143eaf9b030f7d500d
rigel <thoriq.azizasuro@gmail.com>
2020-01-23 11:55:37 +0700

    add login by fb

--- routes/api/login.js
+++ routes/api/login.js
@@ -2,6 +2,7 @@ const express = require('express');
 const jwt = require('jsonwebtoken');
 const keys = require('../../config/keys');
 const axios = require('axios');
+const FB = require("fb");
 const googleLoginUrl = 'https://www.googleapis.com/oauth2/v1';
 const router = express.Router();
 const models = require('../../db/models');
@@ -35,7 +36,7 @@ router.post('/login-google', async (req, res) => {
             });
             
             if (!userData) {
-                res.status(401).json({
+                return res.status(401).json({
                     success: false,
                     errors: `User not registered`,
                     email: email,
@@ -79,4 +80,69 @@ router.post('/login-google', async (req, res) => {
         });
 });
 
+// login by fb 
+router.post('/login-fb', async (req, res) =>{
+    const {token } = req.body;
+    if (!token) return res.status(422).json({
+        success: false,
+        errors: 'Error, Failed to login.'
+    });
+
+    FB.api(
+        "me",
+        {
+          fields: ["id", "name", "address", "birthday", "email", "gender", "picture"],
+          access_token: token
+        },
+        async FBdata => {
+            if(FBdata && FBdata.error ){
+                if (FBdata.error.code === "ETIMEDOUT") return res.status(400).json({ success: false, errors: 'Request timeout!' });
+                return res.status(400).json({
+                    success: false,
+                    errors: FBdata.error.message
+                });
+            }
+
+            const userdata = await models.User.findOne({
+                where: { email: FBdata.email }
+            });
+
+            if(!userdata){
+                return res.status(401).json({
+                    success: false,
+                    errors: `User not registered`,
+                    email: FBdata.email,
+                    name: FBdata.name
+                });
+            }
+
+            let userType = '';
+
+            if (userdata.type === 0 && userdata.companyType === 0) {
+                userType = 'Member';
+            } else if (userdata.type === 0 && userdata.companyType === 1) {
+                userType = 'Company';
+            } else if (userdata.type === 1) {
+                userType = 'Dealer';
+            }
+
+            const payload = {
+                id: userdata.id
+            };
+
+            return jwt.sign(payload, keys.secretKey, { expiresIn: 8 * 3600 }, (err, token) => {
+                if (err) {
+                  errors.jwt = 'Something wrong with JWT signing';
+                  return res.status(404).json(errors);
+                }
+            
+                return res.json({
+                    success: true,
+                    token: `Bearer ${token}`,
+                    userType
+                });
+            });
+        }
+    );
+});
 module.exports = router;
