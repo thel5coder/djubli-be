5559f44ed77664e2b88431f4bd5ac80cb99e04f1
swaraadam <bimaswaraadam@gmail.com>
2020-02-11 13:25:19 +0700

    specific socket origin

--- index.js
+++ index.js
@@ -44,8 +44,8 @@ const io = require('socket.io')(http, {
   handlePreflightRequest: (req, res) => {
     const headers = {
       'Access-Control-Allow-Headers': 'Content-Type, Authorization',
-      'Access-Control-Allow-Origin': req.headers.origin, // or the specific origin you want to give access to,
-      // 'Access-Control-Allow-Origin': https://wine-admin.twisdev.com,
+      // 'Access-Control-Allow-Origin': req.headers.origin, // or the specific origin you want to give access to,
+      'Access-Control-Allow-Origin': `https://apidev-djublee.twisdev.com`,
       'Access-Control-Allow-Credentials': true
     };
     res.writeHead(200, headers);
