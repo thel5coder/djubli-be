24273e9e73f8632214f4b4de517328bd8f05ed56
swaraadam <bimaswaraadam@gmail.com>
2020-02-20 15:46:44 +0700

    change io config

--- index.js
+++ index.js
@@ -34,26 +34,26 @@ const upload = multer({
 const port = process.env.PORT || 5000;
 const app = express();
 const http = require('http').Server(app);
-const io = require('socket.io')(http);
-// const io = require('socket.io')(http, {
-//   path: '/socket',
-//   serveClient: false,
-//   // below are engine.IO options
-//   pingInterval: 10000,
-//   pingTimeout: 5000,
-//   cookie: false,
-//   handlePreflightRequest: (req, res) => {
-//     const headers = {
-//       'Access-Control-Allow-Headers': 'Content-Type, Authorization',
-//       // 'Access-Control-Allow-Origin': req.headers.origin, // or the specific origin you want to give access to,
-//       // 'Access-Control-Allow-Origin': `https://apidev-djublee.twisdev.com`,
-//       'Access-Control-Allow-Origin': req.header('host'),
-//       'Access-Control-Allow-Credentials': true
-//     };
-//     res.writeHead(200, headers);
-//     res.end();
-//   }
-// });
+// const io = require('socket.io')(http);
+const io = require('socket.io')(http, {
+  path: '/socket',
+  serveClient: false,
+  // below are engine.IO options
+  pingInterval: 10000,
+  pingTimeout: 5000,
+  cookie: false,
+  handlePreflightRequest: (req, res) => {
+    const headers = {
+      'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+      // 'Access-Control-Allow-Origin': req.headers.origin, // or the specific origin you want to give access to,
+      'Access-Control-Allow-Origin': `https://apidev-djublee.twisdev.com`,
+      // 'Access-Control-Allow-Origin': req.header('host'),
+      'Access-Control-Allow-Credentials': true
+    };
+    res.writeHead(200, headers);
+    res.end();
+  }
+});
 
 // for dynamic route
 const routes = require('./lib/readfile').walkSync('./routes');
@@ -77,15 +77,15 @@ client.on('error', err => {
   console.log(`Error ${err}`);
 });
 
-app.get('/socket-message', function(req, res){
-  res.sendFile(__dirname + '/socket-message.html');
+app.get('/socket-message', (req, res) => {
+  res.sendFile(`${__dirname}/socket-message.html`);
 });
 
-io.on('connection', (socket) => {
+io.on('connection', socket => {
   console.log('client connected');
 
-  socket.on('negotiation-car', (msg) => {
-    console.log(`Message: ${msg}`)
+  socket.on('negotiation-car', msg => {
+    console.log(`Message: ${msg}`);
   });
 });
 
