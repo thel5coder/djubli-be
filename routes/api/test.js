8ba669b766fb5cac9d9977817907224f9c9d9527
Agus Bintoro <bintoro@twiscoder.com>
2020-04-28 15:56:06 +0700

    test push notif tabNego

--- routes/api/test.js
+++ routes/api/test.js
@@ -79,4 +79,58 @@ router.post(
   }
 );
 
+router.post('/negotiate', passport.authenticate('user', { session: false }), async (req, res) => {
+  const { id } = req.user;
+  // const { roomId } = req.body;
+  const { title, body, action, carId, category, status } = req.body;
+  let { socketId, attributeId } = req.body;
+  socketId = socketId ? socketId : `notification`;
+
+  const carExist = await models.Car.findByPk(carId, {
+    include: [
+      {
+        model: models.Room,
+        as: 'room',
+        include: [
+          {
+            required: false,
+            model: models.RoomMember,
+            as: 'members',
+            where: {
+              userId: { [Op.ne]: id }
+            }
+          }
+        ]
+      }
+    ]
+  });
+  if (!carExist) return res.status(404).json({ success: false, errors: 'car not found' });
+  if (!carExist.roomId) return res.status(422).json({ success: false, errors: 'room null' });
+  if (carExist.room.members.length < 1)
+    return res.status(422).json({ success: false, errors: 'member is null' });
+  attributeId = carExist.room.members[0].userId;
+
+  // return res.status(200).json({ success: true, data: carExist });
+  const data = { id: carId ? carId : 13 };
+  const userNotif = {
+    userId: id,
+    collapseKey: null,
+    notificationTitle: title ? title : `Car Sell`,
+    notificationBody: body ? `${body} #${data.id}` : `Car Sell #${data.id}`,
+    notificationClickAction: action ? action : `carSell`,
+    dataReferenceId: data.id,
+    category: category ? category : 1,
+    status: status ? status : 1
+  };
+  const emit = await notification.insertNotification(userNotif);
+  req.io.emit(`${socketId}-${attributeId}`, emit);
+  notification.userNotif(userNotif);
+
+  return res.status(200).json({
+    success: true,
+    data: `parameter oke`,
+    emit
+  });
+});
+
 module.exports = router;
